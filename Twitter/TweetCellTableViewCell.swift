d7fcd9bc50248a333915c834baa94c20709a1055
Bryan Lopez <bryanlopezrr@gmail.com>
2019-11-09 01:09:22 -0500

    added: favoriting tweets

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -23,6 +23,47 @@ class TweetCellTableViewCell: UITableViewCell {
 //    }
 //    
     
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    func setFavorited(_ isFavorited:Bool){
+            favorited = isFavorited
+        if(favorited){
+            favoriteButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else{
+            favoriteButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+        
+    }
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    @IBAction func retweetAction(_ sender: Any) {
+    }
+    
+    @IBAction func favoriteAction(_ sender: Any) {
+        let tobeFavorited = !favorited
+        
+        if(tobeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorited(true)
+            }, failure: { (Error) in
+                print("Couldnt favorite your tweet. Oh no! \(Error)")
+            })
+        }
+        else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorited(false)
+            }, failure: { (Error) in
+                print("Coulnt unfavorite your tweet. Oh no! \(Error)")
+            })
+        }
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
